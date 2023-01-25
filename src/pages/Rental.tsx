4613f1f47191be230950184aa9ddba98ffa765a6
정채윤 <jeongchaeyoon@jeongchaeyun-ui-MacBookAir.local>
2023-01-26 00:26:41 +0900

    신청서 작성 틀 완성

--- src/pages/Rental.tsx
+++ src/pages/Rental.tsx
@@ -1,7 +1,14 @@
+import React, {useState} from 'react';
 import styled from 'styled-components';
 import Menubar from '../components/Menubar'
 
 const Rental = () => {
+    const selectList = ["apple", "banana", "grape", "orange"];
+    const [Selected, setSelected] = useState("");
+
+    const handleSelect = (e: { target: { value: React.SetStateAction<string>; }; }) => {
+        setSelected(e.target.value);
+    };
     return (
         <>
         <Menubar/>
@@ -12,13 +19,61 @@ const Rental = () => {
             </_Listwrap>
             <_Writewrap>
                 <_Subtext>신청서 작성</_Subtext>
-                <_Inputtitle>이름</_Inputtitle>
-                <_Inputtitle>학번</_Inputtitle>
-                <_Inputtitle>이용인원</_Inputtitle>
-                <_Inputtitle>대여기간</_Inputtitle>
-                <_Inputtitle>불출시점</_Inputtitle>
-                <_Inputtitle>연락처</_Inputtitle>
+                <_Inputtitle>이름<_Input placeholder='이름을 입력해주세요.' width={"150px"}/></_Inputtitle>
+                <_Inputtitle>학번<_Input placeholder='학번을 입력해주세요.' width={"150px"}/></_Inputtitle>
+                <_Inputtitle>이용인원<_Input placeholder='9' width={"35px"}/>명</_Inputtitle>
+                <_Inputtitle>대여기간
+                    <_Dropdownwrap>
+                    <Dropdown onChange={handleSelect} value={Selected}>
+                        {selectList.map((item) => (
+                        <option value={item} key={item}>
+                            {item}
+                        </option>
+                        ))}
+                    </Dropdown>
+                        <_Line1/>
+                    <Dropdown onChange={handleSelect} value={Selected}>
+                        {selectList.map((item) => (
+                        <option value={item} key={item}>
+                            {item}
+                        </option>
+                        ))}
+                    </Dropdown>
+                    </_Dropdownwrap>
+                </_Inputtitle>
+                <_Inputtitle>불출시점
+                <_Dropdownwrap>
+                    <Dropdown onChange={handleSelect} value={Selected}>
+                        {selectList.map((item) => (
+                        <option value={item} key={item}>
+                            {item}
+                        </option>
+                        ))}
+                    </Dropdown>
+                        <_Line1/>
+                    <Dropdown onChange={handleSelect} value={Selected}>
+                        {selectList.map((item) => (
+                        <option value={item} key={item}>
+                            {item}
+                        </option>
+                        ))}
+                    </Dropdown>
+                    </_Dropdownwrap>
+                </_Inputtitle>
+                <_Inputtitle>연락처<_Input placeholder='연락처를 입력해주세요.' width={"150px"}/></_Inputtitle>
+                <_Input2title>이용목적 및 이용내역
+                    <_Input2wrap>
+                        <_Subinputtitle>※사용 용도, 작품 내용 등을 구체적으로 적어주세요.</_Subinputtitle>
+                        <_Input2>- 이용목적: (예: 영화콘텐츠 제작, 미디어교육, 행사)
+                            - 촬영일자:  월  일
+                            - 사용장소:
+                            - 제작내용:
+                        </_Input2>
+                    </_Input2wrap>
+                </_Input2title>
+                            <Btnwrap><_SubmitBtn bg="#1E00D3" color="#ffffff">제출하기</_SubmitBtn><_SubmitBtn bg="#ffffff" color="#000000">취소</_SubmitBtn></Btnwrap>
             </_Writewrap>
+            
         </_Wrap>
         </>
     );
@@ -90,4 +145,101 @@ const _Inputtitle = styled.div`
     padding-bottom: 20px;
     padding-left: 10px;
     box-shadow: inset 0 -1px 0 #888888;
+    display: flex;
+`
+
+const _Input = styled.input`
+    width:${props => props.width};
+    height: 20px;
+    margin-left: 20px;
+    border: 1px solid #888888;
+    border-radius: 3px;
+    padding-left: 5px;
+`
+
+const _Dropdownwrap = styled.div`
+    margin-left: 20px;
+    display: flex;
+    align-items: center;
+`
+
+const Dropdown = styled.select`
+    width: 80px;
+    height: 25px;
+`
+
+const _Line1 = styled.div`
+    width: 8px;
+    height: 1.3px;
+    background-color: #000000;
+    margin:0 10px 0 10px;
+`
+
+const _Input2title = styled.div`
+    margin-top: 20px;
+    font-size: 20px;
+    font-weight: 500;
+    padding-bottom: 20px;
+    padding-left: 10px;
+    box-shadow: inset 0 -1px 0 #888888;
+    display: flex;
+    @media (max-width: 600px) {
+    flex-direction: column;
+    }
+`
+
+const _Input2wrap = styled.div`
+    display: flex;
+    flex-direction: column;
+`
+
+const _Subinputtitle = styled.span`
+    font-size: 16px;
+    margin-left: 10px;
+    margin-top: 3px;
+    color: red;
+    @media (max-width: 600px) {
+    font-size: 15px;
+    margin-left: 0;
+    }
+`
+
+const _Input2 = styled.textarea`
+    width: 300px;
+    height: 200px;
+    margin-left: 20px;
+    border: 1px solid #888888;
+    border-radius: 3px;
+    padding-left: 5px;
+    white-space: pre-line;
+    @media (max-width: 600px) {
+    margin-left: 0;
+    font-size: 13px;
+    }
+`
+
+const Btnwrap = styled.div`
+    display: flex;
+    justify-content: center;
+    button:last-child {
+        border: 1px solid #888888;
+    }
+`
+interface Container{
+    bg: any;
+    color: any;
+}
+const _SubmitBtn = styled.button<Container>`
+    width: 25vw;
+    height: 60px;
+    background: ${props => props.bg};
+    color : ${props => props.color};
+    border-radius: 15px;
+    border: none;
+    margin: 5px;
+    margin-top: 15px;
+    font-size: 16px;
+    @media (max-width: 600px) {
+    font-size: 13px;
+    }
 `
